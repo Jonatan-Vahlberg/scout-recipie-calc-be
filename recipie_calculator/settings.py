1dcce99bb2e8edf647dbe69fa2618ced9a1f5548
Jonatan <jonatan.vahlberg@willandskill.se>
2022-01-09 21:24:03 +0100

    changed Procfile name

--- recipie_calculator/settings.py
+++ recipie_calculator/settings.py
@@ -134,6 +134,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+# Comment for commit message
 django_heroku.settings(locals(), staticfiles=False, allowed_hosts=False)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
